ce81f198157561fa340d73d9b4a90cea0c651ae7
Chisanan232 <chi10211201@cycu.org.tw>
2026-09-14 14:17:30 +0800

    ♻️ (config): Rename the startup matcher property to SceneMux.

--- Sources/AppBundle/config/parseOnWindowDetected.swift
+++ Sources/AppBundle/config/parseOnWindowDetected.swift
@@ -31,7 +31,7 @@ struct WindowDetectedCallbackMatcher: ConvenienceCopyable, Equatable {
     var appNameRegexSubstring: Regex<AnyRegexOutput>?
     var windowTitleRegexSubstring: Regex<AnyRegexOutput>?
     var workspace: String?
-    var duringWinMuxStartup: Bool?
+    var duringSceneMuxStartup: Bool?
 
     var debugJson: Json {
         var resultParts: [String] = []
@@ -47,8 +47,8 @@ struct WindowDetectedCallbackMatcher: ConvenienceCopyable, Equatable {
         if let workspace {
             resultParts.append("workspace=\"\(workspace)\"")
         }
-        if let duringWinMuxStartup {
-            resultParts.append("duringWinMuxStartup=\(duringWinMuxStartup)")
+        if let duringSceneMuxStartup {
+            resultParts.append("duringSceneMuxStartup=\(duringSceneMuxStartup)")
         }
         return .string(resultParts.joined(separator: ", "))
     }
@@ -75,7 +75,7 @@ private let matcherParsers: [String: any ParserProtocol<WindowDetectedCallbackMa
     "workspace": Parser(\.workspace, upcast(parseString)),
     "app-name-regex-substring": Parser(\.appNameRegexSubstring, upcast(parseCasInsensitiveRegex)),
     "window-title-regex-substring": Parser(\.windowTitleRegexSubstring, upcast(parseCasInsensitiveRegex)),
-    "during-winmux-startup": Parser(\.duringWinMuxStartup, upcast(parseBool)),
+    "during-winmux-startup": Parser(\.duringSceneMuxStartup, upcast(parseBool)),
 ]
 
 private func upcast<T>(_ fun: @escaping @Sendable (TOMLValueConvertible, TomlBacktrace) -> ParsedToml<T>) -> @Sendable (TOMLValueConvertible, TomlBacktrace) -> ParsedToml<T?> {
