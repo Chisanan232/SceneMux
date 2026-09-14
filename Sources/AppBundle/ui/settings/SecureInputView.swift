e01b8cc28652cc29a59c37507b9ec18588fa5fd0
Chisanan232 <chi10211201@cycu.org.tw>
2026-09-14 14:16:14 +0800

    ♻️ (settings): Say SceneMux in the Secure Input explanation.

--- Sources/AppBundle/ui/settings/SecureInputView.swift
+++ Sources/AppBundle/ui/settings/SecureInputView.swift
@@ -54,7 +54,7 @@ struct SecureInputView: View {
                     .aspectRatio(contentMode: .fit)
                     .padding(6)
             } else {
-                Text("WinMux cannot respond to keyboard shortcuts while **Secure Input** is active. **Secure Input** is a macOS security feature that prevents applications from reading keyboard events.")
+                Text("SceneMux cannot respond to keyboard shortcuts while **Secure Input** is active. **Secure Input** is a macOS security feature that prevents applications from reading keyboard events.")
                     .font(.title3)
                     .padding(10)
             }
